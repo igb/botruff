75b6ca8812a56ae5d4f4a0d19785de57d2e6ec13
Ian Brown <ibrown@netflix.com>
2024-09-22 20:58:50 -0700

    oops, and the run script

---
+++ run.sh
@@ -0,0 +1,3 @@
+./build.sh
+java -cp lib/polyglot-24.1.0.jar:lib/python-language-24.1.0.jar:lib/truffle-api-24.1.0.jar:lib/bcprov-jdk18on-1.78.1.jar:lib/bcpkix-jdk18on-1.78.1.jar:dist/ org.hccp.graal.python.Test
+
