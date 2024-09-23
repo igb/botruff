b957d3441c0a59200c1d3e8a1b244d283799c0ea
Ian Brown <ibrown@netflix.com>
2024-09-22 20:56:07 -0700

    adding first bouncy castle classpath dep

--- src/org/hccp/graal/python/Test.java
+++ src/org/hccp/graal/python/Test.java
@@ -1,10 +1,13 @@
-package org.hccp.hraal.python;
+package org.hccp.graal.python;
 
 import org.graalvm.polyglot.*;
 
 
-public class Test {
-
-
+public class Test  {
+    public static void main(String[] args) throws Exception {
+	Context context = Context.create();
+	System.out.println(context.eval("python", "'Hello Python!'").asString());
+    }
+    
 
 }
